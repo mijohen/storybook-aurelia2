19e647b8a85767bfff5341a45ccbd6c5f35a05b6
Michael Henderson <michael-henderson@live.com.au>
2024-01-05 11:58:04 +1000

    Fix

--- workspaces/aurelia2-webpack5/src/preset.ts
+++ workspaces/aurelia2-webpack5/src/preset.ts
@@ -1,5 +1,5 @@
 import { dirname, join } from 'path';
-import type { PresetProperty } from '@storybook/types';
+import type { PresetProperty, CoreConfig, StorybookConfigOptions } from '@storybook/types';
 import type { StorybookConfig } from './types';
 
 const getAbsolutePath = <I extends string>(input: I): I =>
@@ -9,7 +9,7 @@ export const addons: PresetProperty<'addons', StorybookConfig> = [
     getAbsolutePath('@mijohen/storybook-preset-aurelia2-webpack'),
 ];
 
-export const core: PresetProperty<'core', StorybookConfig> = async (config, options) => {
+export const core: PresetProperty<'core', StorybookConfig> = async (config: CoreConfig | undefined, options: StorybookConfigOptions) => {
     const framework = await options.presets.apply<StorybookConfig['framework']>('framework');
 
     return {
